807cb49ab265dcad91d15233960afb7e7db1686f
220887 <220887@K5-305-1685250>
2024-04-16 13:00:37 +0400

    1

---
+++ Entities/Items/DefaultItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3inRowGame.Entities.Items
+{
+    enum DefaultItemType
+    {
+        helm,
+        treasure,
+        clover,
+        crown,
+    }
+    class DefaultItem : Item
+    {
+        public DefaultItem(DefaultItemType type) : base()
+        {
+
+        }
+        protected override string GetImagePath()
+        {
+            
+        }
+    }
+}
